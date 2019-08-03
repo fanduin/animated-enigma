2aab91fec6b9674d1a9ecf9becb89c40ec59cd11
Cian Nugent McGealy <cnugentmcgealy@gmail.com>
2019-08-03 16:01:06 +0100

    Speed now decays. Speed and time elapsed are now shown on screen.

---
+++ scripts/GUI.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+var time_start = 0
+var time_now = 0
+
+onready var player = get_node("../Player")
+onready var speed_label = get_node("MarginContainer/HBoxContainer/Speed")
+onready var time_label = get_node("MarginContainer/HBoxContainer/Time")
+
+func _ready():
+	time_start = OS.get_unix_time()
+	set_process(true)
+	print(player.position.x)
+
+func _process(delta):
+	time_now = OS.get_unix_time()
+	var elapsed = time_now - time_start
+	var minutes = elapsed / 60
+	var seconds = elapsed % 60
+	speed_label.text = "%d" % player.speed
+	time_label.text = "%02d : %02d" % [minutes, seconds]
---
+++ scripts/Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+export (int) var max_speed = 200
+export (int) var min_speed = 10
+export (int) var speed_decay = 10
+
+var velocity = Vector2()
+onready var speed = max_speed
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+
+func get_input():
+	look_at(get_global_mouse_position())
+	velocity = Vector2(speed, 0).rotated(rotation)
+
+func _physics_process(delta):
+	get_input()
+	speed = clamp(speed - speed_decay * delta, min_speed, max_speed)
+	velocity = move_and_slide(velocity)
